fd378f97b188f877483df658f043ac9ebac2a528
aisprint <143149200+aisprint@users.noreply.github.com>
2024-05-03 17:36:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,134 @@ from dotenv import load_dotenv
 import os
 import tiktoken
 
+import pandas as pd
+import numpy as np
+
+import time
+from datetime import datetime
+
 load_dotenv()
+
+st.set_page_config("Multi-LLM Tester", page_icon=":memo:")
+
+# Custom styles for the author name and updated date
+st.markdown("""
+<style>
+.title-style {
+    font-size: 50px;
+    font-weight: bold;
+    color: #5D00FF;
+    text-align: center;
+    margin-top: 30px;
+}
+.author-style {
+    font-size: 20px;
+    font-weight: bold;
+    color: #D4BCFF;
+    text-align: center;
+    margin-bottom: 5px;
+}
+.update-date-style {
+    font-size: 16px;
+    color: #555;
+    text-align: center;
+    margin-top: 5px;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# Display the stylish title
+st.markdown('<div class="title-style">Multi-LLM Tester</div>', unsafe_allow_html=True)
+
+# Display author and the updated date
+current_date = datetime.now().strftime("%m/%d/%Y")
+st.markdown('<div class="author-style">By Mamoru</div>', unsafe_allow_html=True)
+st.markdown(f'<div class="update-date-style">Last Updated: {current_date}</div>', unsafe_allow_html=True)
+
+# Privacy Policy
+privacy_policy = st.expander("Privacy Policy")
+privacy_policy.write("""
+According to OpenAI and Anthropic, information sent via their APIs is not used for training large language models (LLMs).
+- [OpenAI Documentation](https://platform.openai.com/docs/introduction)
+- [Anthropic Support](https://support.anthropic.com/en/articles/7996875-can-you-delete-data-that-i-sent-via-api)
+""")
+
+
+# Add a new section for how to use the Paper Summarizer
+st.markdown("For instructions on how to use the Paper Summarizer, visit the following link:")
+st.markdown("[How to Use Paper Summarizer](https://github.com/aisprint/paper_summarizer_v2/blob/master/README.md)", unsafe_allow_html=True)
+
+
+# Initialize or update the session state for conversation history
+if 'history' not in st.session_state:
+    st.session_state.history = []
+
+# Sidebar for API keys
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+st.sidebar.markdown(
+    "[How to get OpenAI API key?](https://help.openai.com/en/articles/4936850-where-do-i-find-my-openai-api-key)", 
+    unsafe_allow_html=True
+)
+st.sidebar.markdown(
+    "[OpenAI API Pricing](https://openai.com/api/pricing)", 
+    unsafe_allow_html=True
+)
+
+anthropic_api_key = st.sidebar.text_input('Anthropic API Key', type='password')
+st.sidebar.markdown(
+    "[How to get Anthropic API key?](https://docs.anthropic.com/claude/reference/getting-started-with-the-api)", 
+    unsafe_allow_html=True
+)
+st.sidebar.markdown(
+    "[Anthropic API Pricing](https://www.anthropic.com/api)", 
+    unsafe_allow_html=True
+)
+
+# API Pricing
+data = {
+    "Model": [
+        "claude-3-haiku-20240307", 
+        "claude-3-sonnet-20240229", 
+        "gpt-3.5-turbo", 
+        "gpt-4-turbo", 
+        "claude-3-opus-20240229"
+    ],
+    "Prompt Cost": [
+        "$0.0025", 
+        "$0.03", 
+        "$0.005", 
+        "$0.1", 
+        "$0.15"
+    ],
+    "Completion Cost": [
+        "$0.0025", 
+        "$0.03", 
+        "$0.003", 
+        "$0.06", 
+        "$0.15"
+    ],
+    "Total Cost": [
+        "$0.005", 
+        "$0.06", 
+        "$0.008", 
+        "$0.16", 
+        "$0.3"
+    ]
+}
+
+# dataframe for api pricing
+df_costs = pd.DataFrame(data)
+st.sidebar.title("API cost estimation (Prompt 10,000 tokens, Completion 2,000 Tokens)")
+st.sidebar.table(df_costs)
+
+
+# Function to add conversation to history with formatted title
+def add_to_history(user_input, response, file_name):
+    now = datetime.now()
+    formatted_date = now.strftime("%m/%d/%Y %H:%M:%S")
+    title = f"{formatted_date}"
+    st.session_state.history.append({"title": title, "user_input": user_input, "response": response})
+
 
 # Initialize clients with keys
 
@@ -84,14 +211,17 @@ with st.form(key='my_form'):
 
     if submitted:
         selected_models = [model for model, checked in model_selection.items() if checked]
-        if selected_models:
-            tab_container = st.tabs([f"{model}" for model in selected_models])
-            for i, model in enumerate(selected_models):
-                with tab_container[i]:
-                    if 'claude' in model:
-                        response_text = generate_response_anthropic(prompt, model, temperature, max_tokens)
-                    else:
-                        response_text = generate_response_openai(prompt, model, temperature, max_tokens)
-                    st.text_area("Response:", value=response_text, height=500, disabled=False)
-        else:
-            st.error("Please select at least one model.")
+        with st.spinner('Generating.....🤖💤'):
+            if selected_models:
+                tab_container = st.tabs([f"{model}" for model in selected_models])
+                for i, model in enumerate(selected_models):
+                    with tab_container[i]:
+                        if 'claude' in model:
+                            response_text = generate_response_anthropic(prompt, model, temperature, max_tokens)
+                        else:
+                            response_text = generate_response_openai(prompt, model, temperature, max_tokens)
+                        st.text_area("Response:", value=response_text, height=500, disabled=False)
+                        completion_token_count = num_tokens_from_string(response_text, "cl100k_base")
+                        st.markdown(f"**Completion Token Count:** {completion_token_count}")
+            else:
+                st.error("Please select at least one model.")
